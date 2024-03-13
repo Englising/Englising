d95440b29f260e278f755cbf1d2b4c88bbb4d1c3
손민기 <thsalsrl7531@naver.com>
2024-03-13 16:32:08 +0900

    :sparkles: feat-fe: SinglePage 가사 빈칸 구분기능

--- englising-fe/src/component/single/Lyrics.tsx
+++ englising-fe/src/component/single/Lyrics.tsx
@@ -8,27 +8,42 @@ interface Props {
     answer: string
 }
 
+
 interface Lyric {
-    start_time: number;
-    end_time: number;
+    startTime: number;
+    endTime: number;
     lyric: string[];
+}
+
+interface Blank {
+    singlePlayWordId : number,
+    sentenceIndex : number,
+    wordIndex : number,
+    word : string,
+    isRight : boolean
 }
 
 const Lyrics = ({onSetInfo, playInfo, answer}:Props) => {
     const [lyrics, setLyrics] = useState<Lyric[]>([]);
+    const [blank, setBlank] = useState<Blank[]>([]);
     const scrollRef = useRef<(HTMLDivElement | null)[]>([]);
     const {idx, startTime, endTime, toggle} = playInfo;
     
-    // 실제론 axios를 사용한 호출이 필요함 (sing)
-    const data:Lyric[] = singleData.data.lyrics;
+    // aixos 호출로 데이터 받기 ///////////////////
+    const lyricsData:Lyric[] = singleData.data.lyrics;
+    const blankData:Blank[] = singleData.data.words;
+
     useEffect(() => {
-        setLyrics([...data]);
+        setLyrics([...lyricsData]);
+        setBlank([...blankData]);
     },[])
+
+    /////////////////////////////////////////////
 
     // FootVar에서 답안이 입력되었을 때
     useEffect(() => {
         if(answer === "") return;
-        handleLyricsClick(idx+1, lyrics[idx+1].start_time, lyrics[idx+1].end_time)
+        handleLyricsClick(idx+1, lyrics[idx+1].startTime, lyrics[idx+1].endTime)
     },[answer])
     
     const handleLyricsClick = (currIdx: number, start: number, end: number) => {
@@ -41,17 +56,34 @@ const Lyrics = ({onSetInfo, playInfo, answer}:Props) => {
         // single page -> player (가사의 시작시간, 종료시간)
         onSetInfo(currIdx, start, end);
     }
+    
+    const testClick = () => {
+        lyrics[0].lyric[0]
+        
+    }
     return(
         <div className="h-96 overflow-x-auto">
             {lyrics.map((lyric, i) => {
                 return(
-                    <div key={i} 
+                    <div 
+                    key={i} 
                     className="h-10" 
                     ref={(el) => scrollRef.current[i] = el} 
                     onClick={() => 
-                    handleLyricsClick(i, lyric.start_time, lyric.end_time)}>
-                        {lyric.lyric.map((word, i) => {
-                            return <span key={i}> {word} </span>
+                    handleLyricsClick(i, lyric.startTime, lyric.endTime)}>
+                        {lyric.lyric.map((word, j) => {
+                            let isBlank:boolean = false;
+
+                            blank.forEach((curr) => {
+                                if(word == curr.word) isBlank = true;
+                            })
+                            //만약 해당 단어가 빈칸이 필요하다면 -> isBlank 속성 값 결정
+                            return (
+                                isBlank ? (<span key={j} data-sntIdx={i} data-wdInx={j} className="bg-secondary-800"> 
+                                {word}
+                                </span>) 
+                                : (<span key={j} data-blank={isBlank}> {word} </span>)
+                            );
                         })}
                     </div>
                 );
