7ae54b2823132eca3ef8c01ccab7e05859dd5a1a
hyunji <hinngreen@naver.com>
2024-03-19 17:00:50 +0900

    ✨ feat-fe: 라우터 설정

--- englising-fe/src/App.tsx
+++ englising-fe/src/App.tsx
@@ -1,34 +1,65 @@
 import '../src/index.css'
 import './App.css'
 import {
-  BrowserRouter,
-  Route,
-  Routes,
+  createBrowserRouter,
+  RouterProvider
 } from 'react-router-dom'
 import SelectMultiPage from './pages/SelectMultiPage'
 import IndexPage from './pages/IndexPage'
 import SelectSinglePage from './pages/SelectSinglePage'
-import SettingMulti from './pages/SettingMultiPage'
+import SettingMultiPage from './pages/SettingMultiPage'
 import WaitingRoomPage from './pages/WaitingRoomPage'
-import SinglePage from './pages/SinglePage'
+import SinglePlay from './pages/SinglePage'
+import Multiplay from './pages/MultiplayPage'
+import WordListPage from './pages/WordListPage'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <IndexPage/>,
+    },    
+    {
+      path: "/englising",
+      children: [
+        {
+          path:"selectSingle",
+          element: <SelectSinglePage /> 
+        },
+        {
+          path:"selectMulti",
+          element: <SelectMultiPage /> 
+        },
+        {
+          path:"settingMulti",
+          element: <SettingMultiPage /> 
+        },
+        {
+          path:"wordList",
+          element: <WordListPage /> 
+        }
+      ]
+    },
+    {
+      path: "/singlePlay",
+      element: <SinglePlay/>
+    },
+    {
+      path: "/waitroom/:multiId",
+      element: <WaitingRoomPage/>
+    },
+    {
+      path: "/multiPlay/:multiId",
+      element: <Multiplay/>
+    },
+  ])
+
   return (
     <div>
-      <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path ="/" element= {<SelectMultiPage/>} />
-            <Route path ="/index" element= {<IndexPage/>} />
-            <Route path ="/selectMulti" element= {<SelectMultiPage/>} />
-            <Route path ="/select" element= {<SelectSinglePage/>} />
-            <Route path ="/settingMulti" element= {<SettingMulti/>}/>
-            <Route path ="/waitingroom" element= {<WaitingRoomPage/>} />
-            <Route path ="/singlepage" element= {<SinglePage/>} />
-        </Routes>
+      <div>
+      <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
       </div>
-    </BrowserRouter>
     </div>
   )
 }
