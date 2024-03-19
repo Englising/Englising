9fcffe5d5196d21b09f6d6ddfe31abe29d2de376
이지우 <jwl0103@naver.com>
2024-03-19 17:21:09 +0900

    :sparkles: feat-be: userController 추가

--- englising-be/src/main/java/org/englising/com/englisingbe/user/controller/UserController.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/user/controller/UserController.java
@@ -1,29 +1,13 @@
 package org.englising.com.englisingbe.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.englising.com.englisingbe.global.dto.DefaultResponseDto;
-import org.englising.com.englisingbe.jwt.JwtResponseDto;
-import org.englising.com.englisingbe.user.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/user")
 public class UserController {
 
-    private final UserService userService;
-
-    @PostMapping("/guest")
-    public ResponseEntity<DefaultResponseDto<?>> guest() throws Exception {
-
-        JwtResponseDto jwtResponseDto = userService.guest();
-        // todo. 에러 처리 추가
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(new DefaultResponseDto<>(200, "토큰 발급이 완료되었습니다.", jwtResponseDto));
-    }
 
 }
