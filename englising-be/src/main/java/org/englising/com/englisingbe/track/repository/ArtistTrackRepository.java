66acb97fa48bd900bc96a1ec31ecfa47c974fd8a
AhyngK <130175561+AhyngK@users.noreply.github.com>
2024-03-20 10:31:20 +0900

    🐛 fix-be: repository 수정

--- englising-be/src/main/java/org/englising/com/englisingbe/track/repository/ArtistTrackRepository.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/track/repository/ArtistTrackRepository.java
@@ -3,5 +3,5 @@ package org.englising.com.englisingbe.track.repository;
 import org.englising.com.englisingbe.track.entity.ArtistTrack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArtistTrackRepository extends JpaRepository<Long, ArtistTrack> {
+public interface ArtistTrackRepository extends JpaRepository<ArtistTrack, Long> {
 }
