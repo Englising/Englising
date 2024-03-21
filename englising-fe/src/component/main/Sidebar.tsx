501a80e4f99b484995c59ae34c5b29531ae01e57
hyunji <hinngreen@naver.com>
2024-03-21 09:14:26 +0900

    ✨ feat-fe:: 사이드바 연결 수정

--- englising-fe/src/component/main/Sidebar.tsx
+++ englising-fe/src/component/main/Sidebar.tsx
@@ -29,18 +29,11 @@ const SideBar: React.FC = () => {
     }
 
     // 메뉴 아이템 배열
-    const single_title: MenuItem[] = [
-        { name: '싱글 플레이', path: '/englising/selectSingle' },
-    ];
 
     const single_menus: MenuItem[] = [
         { name: '추천 플레이리스트', path: '/englising/selectSingle' },
         { name: '좋아요한 음악', path: '/englising/selectSingle' },
         { name: '최근 플레이한 음악', path: '/englising/selectSingle' },
-    ];
-
-    const multi_title: MenuItem[] = [
-        { name: '단체 플레이', path: '/englising/selectMulti' },
     ];
 
     const multi_menus: MenuItem[] = [
@@ -80,21 +73,12 @@ const SideBar: React.FC = () => {
                 }
 
                 <div className='pt-6 text-sm text-primary-300'>MUSIC</div>
-                {single_title.map((menu, index) => (
-                    <NavLink
-                        to={menu.path}
-                        key={index}
-                        className="pl-5 pt-3"
-                        style={({ isActive }) => (isActive ? activeStyle : {})}
-                    >
-                        <div className='flex flex-row gap-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
-                            </svg>
-                            {menu.name}
-                        </div>
-                    </NavLink>
-                ))}
+                <div className='flex flex-row gap-2 pl-5 pt-3'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
+                    </svg>
+                    싱글 플레이
+                </div>
                 {single_menus.map((menu, index) => (
                     <NavLink
                         to={menu.path}
@@ -105,21 +89,12 @@ const SideBar: React.FC = () => {
                         {menu.name}
                     </NavLink>
                 ))}
-                {multi_title.map((menu, index) => (
-                    <NavLink
-                        to={menu.path}
-                        key={index}
-                        className="pl-5 pt-5"
-                        style={({ isActive }) => (isActive ? activeStyle : {})}
-                    >
-                        <div className='flex flex-row gap-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                            </svg>
-                            {menu.name}
-                        </div>
-                    </NavLink>
-                ))}
+                <div className='flex flex-row gap-2 pl-5 pt-5'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                    </svg>
+                    단체 플레이
+                </div>
                 {multi_menus.map((menu, index) => (
                     <NavLink
                         to={menu.path}
