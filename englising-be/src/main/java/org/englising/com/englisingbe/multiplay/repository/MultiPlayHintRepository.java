2b9a055641c66fee781fee1004077a95197b5806
고해림 <sunny9612@naver.com>
2024-03-21 10:01:29 +0900

    ✨feat-be: 멀티플레이 Controller 작성

--- englising-be/src/main/java/org/englising/com/englisingbe/multiplay/repository/MultiPlayHintRepository.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/multiplay/repository/MultiPlayHintRepository.java
@@ -3,6 +3,6 @@ package org.englising.com.englisingbe.multiplay.repository;
 import org.englising.com.englisingbe.multiplay.entity.MultiPlayHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MultiPlayHintRepository extends JpaRepository<Long, MultiPlayHint> {
-    MultiPlayHint findByMultiPlayHintId(Long multiPlayHintId);
+public interface MultiPlayHintRepository extends JpaRepository<MultiPlayHint, Long> {
+    MultiPlayHint findByMultiplayHintId(Long multiPlayHintId);
 }
--- englising-be/src/main/java/org/englising/com/englisingbe/multiplay/repository/MultiPlayUserRepository.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/multiplay/repository/MultiPlayUserRepository.java
@@ -3,7 +3,6 @@ package org.englising.com.englisingbe.multiplay.repository;
 import org.englising.com.englisingbe.multiplay.entity.MultiPlayUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MultiPlayUserRepository extends JpaRepository<Long, MultiPlayUser> {
-    MultiPlayUser findByMultiplayId(Long multiPlayId);
+public interface MultiPlayUserRepository extends JpaRepository<MultiPlayUser, Long> {
     MultiPlayUser save(MultiPlayUser multiPlayUser);
 }
