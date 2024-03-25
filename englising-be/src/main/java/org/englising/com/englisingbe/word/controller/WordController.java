0d3e51cf77c43b3b7201d1530bc30a8d27a156aa
이지우 <jwl0103@naver.com>
2024-03-25 09:10:23 +0900

    ✨feat-be: Word getWordList 구현중

--- englising-be/src/main/java/org/englising/com/englisingbe/word/controller/WordController.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/word/controller/WordController.java
@@ -1,6 +1,9 @@
 package org.englising.com.englisingbe.word.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.englising.com.englisingbe.auth.dto.CustomUserDetails;
 import org.englising.com.englisingbe.global.dto.DefaultResponseDto;
@@ -28,12 +31,18 @@ public class WordController {
             summary = "단어장 리스트 조회",
             description = "단어장 목록을 타입별로 (좋아요한 단어, 최근 플레이한 단어, 검색한 단어) 조회합니다"
     )
+    @Parameters({
+            @Parameter(name = "token", description = "JWT AccessToken", in = ParameterIn.COOKIE),
+            @Parameter(name = "type", description = "단어장 종류 : PLAYED(플레이한), LIKE(좋아요 한), SEARCHED(검색한)", in = ParameterIn.QUERY),
+            @Parameter(name = "page", description = "페이지 번호", in = ParameterIn.QUERY),
+            @Parameter(name = "size", description = "(선택적) 페이지당 컨텐츠 개수, 기본 10", in = ParameterIn.QUERY)
+    })
     public ResponseEntity<DefaultResponseDto<WordListResponseDto>> getWordList(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam WordListType type, @RequestParam Integer page, @RequestParam Integer size) {
 
 //        WordListResponseDto wordListResponseDto = wordService.getWordList(type, page, size, userDetails.getUsername());
-        WordListResponseDto wordListResponseDto = wordService.getWordList(type, page, size, "1");
+        WordListResponseDto wordListResponseDto = wordService.getWordList(type, page, size, 1L);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -47,11 +56,17 @@ public class WordController {
             summary = "단어 즐겨찾기",
             description = "단어 즐겨찾기를 등록하고 즐겨찾기 상태를 반환합니다"
     )
+    @Parameters({
+            @Parameter(name = "token", description = "JWT AccessToken", in = ParameterIn.COOKIE),
+            @Parameter(name = "wordId", description = "wordId", in = ParameterIn.QUERY),
+    })
     public ResponseEntity<DefaultResponseDto<?>> likeWord(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                           @RequestParam Long wordId) {
 
-//        WordLikeResponseDto wordLikeResponseDto = wordService.likeWord(wordId, userDetails.getUsername());
-        WordLikeResponseDto wordLikeResponseDto = wordService.likeWord(1L, "1");
+//        Long userId = Long.parseLong(userDetails.getUsername());
+//        WordLikeResponseDto wordLikeResponseDto = wordService.likeWord(wordId, userId);
+
+        WordLikeResponseDto wordLikeResponseDto = wordService.likeWord(1L, 1L);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
--- englising-be/src/main/java/org/englising/com/englisingbe/word/service/WordService.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/word/service/WordService.java
@@ -14,6 +14,10 @@ import org.englising.com.englisingbe.word.entity.Word;
 import org.englising.com.englisingbe.word.entity.WordLike;
 import org.englising.com.englisingbe.word.repository.WordLikeRepository;
 import org.englising.com.englisingbe.word.repository.WordRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,20 +34,45 @@ public class WordService {
     public Word getWordById(long wordId) {
         Word word = wordRepository.findById(wordId)
                 .orElseThrow(() -> new GlobalException(ErrorHttpStatus.WORD_NOT_FOUND));
-
         return word;
     }
 
-    //todo.
-    public WordListResponseDto getWordList(WordListType type, Integer page, Integer size, String userId) {
-     return null;
+    public WordListResponseDto getWordList(WordListType type, Integer page, Integer size, Long userId) {
+        switch (type) {
+            case like -> {
+                return getLikedWords(page, size, userId);
+            }
+            case played -> {
+                return getPlayedWords(page, size, userId);
+            }
+            case searched -> {
+                return getSearchedWords(page, size, userId);
+            }
+        }
+        return null;
     }
 
-    public WordLikeResponseDto likeWord(Long wordId, String userId) {
-        WordLike wordLike = wordLikeRepository.findByWordIdAndUserId(wordId, Long.valueOf(userId)).orElse(null);
+    private WordListResponseDto getPlayedWords(Integer page, Integer size, Long userId) {
+        return null;
+    }
+
+    private WordListResponseDto getLikedWords(Integer page, Integer size, Long userId) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "updatedAt", "createdAt"));
+//        Page<WordLike> wordLikes = wordLikeRepository
+        return null;
+    }
+
+    private WordListResponseDto getSearchedWords(Integer page, Integer size, Long userId) {
+        // todo. 추후 가능하면 (검색한 단어)
+        return null;
+    }
+
+
+    public WordLikeResponseDto likeWord(Long wordId, Long userId) {
+        WordLike wordLike = wordLikeRepository.findByWordIdAndUserId(wordId, userId).orElse(null);
 
         if (wordLike == null) {
-            User user = userService.getUserById(Long.parseLong(userId));
+            User user = userService.getUserById(userId);
             Word word = getWordById(wordId);
 
             WordLike savedWordlike = WordLike.builder()
