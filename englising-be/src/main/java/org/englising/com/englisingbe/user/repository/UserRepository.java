a875031418d9ef46cbfb94f7d0e67dcda326c377
이지우 <jwl0103@naver.com>
2024-03-27 16:21:04 +0900

    🔧 chore-be: cookieUtil에 setDomain 추가

--- englising-be/src/main/java/org/englising/com/englisingbe/user/repository/UserRepository.java
+++ englising-be/src/main/java/org/englising/com/englisingbe/user/repository/UserRepository.java
@@ -10,7 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(Long userId);
     Optional<User> findByEmail(String email);
-    boolean existsByNickname(String nickname);
     boolean existsByNicknameAndUserIdNot(String nickname, Long userId);
 
     int countByNicknameStartingWith(String nickname);
