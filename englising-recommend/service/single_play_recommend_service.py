456a9e9f8570eea6c5f11d3cb1c19d12eabc40aa
고해림 <sunny9612@naver.com>
2024-03-28 13:56:57 +0900

    ✨feat-be: 추천시스템 FastApi 연결

--- englising-recommend/service/single_play_recommend_service.py
+++ englising-recommend/service/single_play_recommend_service.py
@@ -1,17 +1,22 @@
+import random
+from typing import List
+
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sqlalchemy import desc
 
+from crud import track_crud
 from database.mysql_manager import Session
 from model.track import Track
 from model.track_like import TrackLike
 
 session = Session()
 
-def get_feature_data(session):
-    result = session.query(Track.title, Track.track_id, Track.spotify_id, Track.spotify_popularity, Track.feature_acousticness, Track.feature_danceability, Track.feature_energy).all()
+def get_feature_data(session: Session) -> List[Track]:
+    result = track_crud.get_all_tracks(session)
     return result
+
 
 def get_title_from_track_id(track_id, music_df):
     return music_df[music_df.track_id == track_id]["title"].values[0]
@@ -26,16 +31,17 @@ def get_user_liked_tracks(session, user_id, limit=3):
     track_ids = [track_like[0] for track_like in liked_tracks]
     return track_ids
 
-def get_recommend_from_user_id(user_id, limit=10):
+def get_recommend_from_user_id(user_id, limit=18):
     user_likes = get_user_liked_tracks(session, user_id)
     recommendations = get_recommendations(session, user_likes=user_likes, limit=limit)
     return recommendations
 
 def get_recommendations(session, user_likes=[], limit=10):
     music_df = pd.DataFrame(get_feature_data(session))  # music_df 생성
+    # print("music_df", music_df)
 
     if len(user_likes) < 3:
-        popular_tracks = get_popular_tracks(session, limit=limit)
+        popular_tracks = get_popular_tracks(session, limit=18)
         return [track[0] for track in popular_tracks]
 
     features = ['feature_acousticness', 'feature_danceability', 'feature_energy']
@@ -43,13 +49,10 @@ def get_recommendations(session, user_likes=[], limit=10):
         music_df[feature] = music_df[feature].fillna('')
 
     def combine_features(row):
-        try:
-            # print(row['feature_acousticness'], row['feature_danceability'], row['feature_energy'])
-            return str(row['feature_acousticness']) + " " + str(row['feature_danceability']) + " " + str(row['feature_energy'])
-        except:
-            print("Error: ", music_df)
+        return str(row['feature_acousticness']) + " " + str(row['feature_danceability']) + " " + str(row['feature_energy'])
 
     music_df["combine_features"] = music_df.apply(combine_features,axis=1)
+    # print("angry!!!!!!!", music_df)
 
     cv = CountVectorizer()
     count_matrix = cv.fit_transform(music_df["combine_features"])
@@ -57,27 +60,18 @@ def get_recommendations(session, user_likes=[], limit=10):
 
     similar_tracks = []
     for track_id in user_likes:
-        music_user_likes_title = get_title_from_track_id(track_id, music_df)
-        print("사용자가 좋아하는 노래:", music_user_likes_title)
-
+        if music_df[music_df['track_id'] == track_id].empty:
+            continue
         music_user_likes_index = music_df[music_df['track_id'] == track_id].index.values[0]
+
         similar_music = list(enumerate(cosine_sim[music_user_likes_index]))
         sorted_similar_music = sorted(similar_music, key=lambda x: x[1], reverse=True)[1:]
 
-        for track in sorted_similar_music[:5]:
+        for track in sorted_similar_music[:6]:
             track_index = track[0]
             track_id = music_df.iloc[track_index]['track_id']
             similar_tracks.append(track_id)
 
     unique_similar_tracks = list(set(similar_tracks))
+    random.shuffle(unique_similar_tracks)
     return unique_similar_tracks[:limit]
-
-# 사용자의 ID가 user_id인 경우
-user_id = 1  # 사용자의 ID에 맞게 수정
-user_likes = get_user_liked_tracks(session, user_id)
-print("사용자가 좋아하는 노래!!!!!!!!!!", user_likes)
-
-recommendations = get_recommendations(session, user_likes=user_likes, limit=10)
-print("추천하는 유사한 노래:")
-for track in recommendations:
-    print(track)
