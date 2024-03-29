890c6ccfc5bd0e45c3d3bf8fb83b75a35aa308ee
AhyngK <130175561+AhyngK@users.noreply.github.com>
2024-03-29 10:39:01 +0900

    :wrench: dockerfile 수정
    Former-commit-id: 1e780665951047ab21e3bb243ea7416a06d225f5 [formerly f780ee7fb022fcef319c5bd4282099c59ff7b83d]
    Former-commit-id: 8c8c697ae796b1ae8ec7cf6a0788a8082194c03a
    Former-commit-id: fde05e3e975515b0b76eda4b555a4441ad8ebf36

--- englising-recommend/config/fast_text_config.py
+++ englising-recommend/config/fast_text_config.py
@@ -1,6 +1,6 @@
 from gensim.models import KeyedVectors
 
 
-fast_text_model = KeyedVectors.load_word2vec_format('./word_model/wiki-news-300d-1M.vec')
+fast_text_model = KeyedVectors.load_word2vec_format('./word_model/wiki-news-300d-1M-subword.vec')
 print("imported model")
 
