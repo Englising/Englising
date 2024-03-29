e9fd428e18ca754e34be7e66aa9fd4be02a589af
최은비 <ii319@naver.com>
2024-03-29 17:44:10 +0900

    ✨ feat-fe: 채팅 유저 구분 구현 중

--- englising-fe/src/component/chat/ChatMessage.tsx
+++ englising-fe/src/component/chat/ChatMessage.tsx
@@ -1,27 +1,29 @@
 import { User } from "../../pages/MultiplayPage";
+import { Chat } from "./ChatArea";
 
 interface ChatMessageProps {
-  user: User;
-  message: string;
+  chat: Chat;
   profileVisible: boolean;
   myMessage: boolean;
 }
 
-function ChatMessage({ user, message, profileVisible, myMessage }: ChatMessageProps) {
+function ChatMessage({ chat, profileVisible, myMessage }: ChatMessageProps) {
   return (
     <div className={`flex gap-2 ${myMessage ? "justify-end" : ""}`}>
       <div className={`w-8 h-8 flex-shrink-0 rounded-full overflow-hidden ${profileVisible ? "" : "hidden"}`}>
-        <img src={user.profileImage} />
+        {/* <img src={chat.user.profileImage} /> */}
       </div>
       <div>
-        <p className={`mb-1 ${profileVisible ? "" : "hidden"}`}>{user.nickname}</p>
+        {/* <p className={`mb-1 ${profileVisible ? "" : "hidden"}`}>{chat.user.nickname}</p> */}
         {myMessage ? (
-          <p className={`max-w-52 ms-auto py-1 px-2 bg-gray-800 rounded-lg rounded-tr-sm break-keep`}>{message}</p>
+          <p className={`max-w-52 ms-auto py-1 px-2 text-white bg-gray-800 rounded-lg rounded-tr-sm break-keep`}>
+            {chat.message}
+          </p>
         ) : (
           <p
             className={`max-w-52 py-1 px-2 bg-gray-100 text-gray-900 rounded-lg rounded-tl-sm break-keep ${profileVisible ? "" : "ms-10"}`}
           >
-            {message}
+            {chat.message}
           </p>
         )}
       </div>
