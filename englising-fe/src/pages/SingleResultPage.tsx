08f4018c4aedd52fe6789003542856a4efc2dda8
손민기 <thsalsrl7531@naver.com>
2024-03-30 15:58:32 +0900

    :sparkles: feat-fe: 싱글플레이 결과 페이지 생성

---
+++ englising-fe/src/pages/SingleResultPage.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const SingleResultPlage = () => {
+    // 1. 싱글플레이 아이디 넘어와야 함 (이건 쿼리스트링)
+    // 2. 똑같이 배경이랑 타이틀 이름 넘어와야 함 (location의 데이터로)
+    // 3. 뒤로가기 막아보기 - 후작업
+    useEffect(() => {
+
+    }, [])
+    
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+export default SingleResultPlage;
